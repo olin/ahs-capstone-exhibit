95e0f6657a882be0c2534286114c2e8adada6fff
Matthew Brucker <mpbrucker@gmail.com>
2020-04-22 17:04:30 -0400

    Get comment highlighting working.

--- js/comment.js
+++ js/comment.js
@@ -3,7 +3,14 @@ var comments = document.getElementsByClassName("Page__comment");
 
 function onClick (e) {
     var id_num = this.id.split("-")[2];
-    var matching_comment_id = "comment-" + id_num;
+    var matching_comment_id = "comment-body-" + id_num;
+    this.style.backgroundColor ="#ffe376";
+    document.getElementById(matching_comment_id).style.backgroundColor = "#ffe376";
+}
+
+function onClickBody (e) {
+    var id_num = this.id.split("-")[2];
+    var matching_comment_id = "comment-link-" + id_num;
     this.style.backgroundColor ="#ffe376";
     document.getElementById(matching_comment_id).style.backgroundColor = "#ffe376";
 }
@@ -12,22 +19,21 @@ function clickClear(e) {
     var target_id = e.target.id.split("-")[2];
     for (var i = 0; i < comments.length; i++ ) {
         if (comments[i].id.split("-")[2] != target_id) {
-            this.style.backgroundColor ="#fffdbe";
+            comments[i].style.backgroundColor ="#fffdbe";
         }
     }
 
     for (var j = 0; j < comment_links.length; j++ ) {
         if (comment_links[j].id.split("-")[2] != target_id) {
-            this.style.backgroundColor ="#fffdbe";
+            comment_links[j].style.backgroundColor ="#fffdbe";
         }
     }
 }
 
 for (var i = 0; i < comments.length; i++) {
     var className = i % 2 ? "left" : "right";
-    console.log(comments[i]);
-    console.log(i % 1);
     comments[i].classList.add(className);
+    comments[i].onclick = onClickBody;
 }
 
 for (var j = 0; j < comment_links.length; j++) {
