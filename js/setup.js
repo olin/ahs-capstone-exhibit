5d3046fbf219629f66a03e9812fdc91ea499fa3e
Matthew Brucker <mpbrucker@gmail.com>
2020-04-27 23:02:12 -0400

    Scaling up model.

--- js/setup.js
+++ js/setup.js
@@ -19,8 +19,35 @@ AFRAME.registerComponent('prox', {
             }
         } else if (this.inProx) {
             this.inProx = false;
-            console.log('leave');
             this.el.setAttribute('text', {value: ''});
         }
     }
 });
+
+AFRAME.registerComponent('proxlink', {
+    schema: {
+        href: {type: 'string', default: '/'},
+    },
+
+    init: function() {
+        this.cam = document.querySelector('a-camera');
+        this.inProx = false;
+        console.log('test');
+        this.el.addEventListener('click', function(e) {
+            console.log('test');
+        })
+    },
+
+    tick: function(t, dt) {
+        let camPos = this.cam.object3D.position;
+        let elPos = this.el.object3D.position;
+        if (elPos.distanceTo(camPos) < this.data.dist) {
+            if (!this.inProx) {
+                this.inProx = true;
+            }
+        } else if (this.inProx) {
+            this.inProx = false;
+        }
+    }
+});
+
