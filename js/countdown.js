9546d0a1b1e695186876351049d93abbb38bd9fb
Matthew Brucker <mpbrucker@gmail.com>
2020-05-01 10:26:36 -0400

    updating splash screen

---
+++ js/countdown.js
@@ -0,0 +1,53 @@
+var wrapper = document.getElementById("page-wrapper");
+var splash = document.getElementById("splash");
+var timer = document.getElementById("timer");
+
+var origDate = new Date("May 1, 2020 10:26:00");
+
+function changeTimezone(date, ianatz) {
+
+    // suppose the date is 12:00 UTC
+    var invdate = new Date(date.toLocaleString('en-US', {
+        timeZone: ianatz
+    }));
+
+    // then invdate will be 07:00 in Toronto
+    // and the diff is 5 hours
+    var diff = date.getTime() - invdate.getTime();
+
+    // so 12:00 in Toronto is 17:00 UTC
+    return new Date(date.getTime() + diff);
+
+}
+
+var countDownDate = changeTimezone(origDate, "America/New_York");
+
+
+// Update the count down every 1 second
+var countdown = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  timer.innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(countdown);
+    splash.style.opacity = 0;
+    wrapper.style.display = "block";
+  } else {
+    splash.style.visibility = "visible";
+  }
+}, 1000);
